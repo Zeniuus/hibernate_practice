79af7308e1ab8c7b381d347ed8b75899339e2518
Swan <jsh56son@gmail.com>
2019-04-14 13:45:06 +0900

    Initial commit: project setup

---
+++ src/main/kotlin/kr/co/vcnc/swan/study/hibernate/practice/HibernatePracticeApplication.kt
@@ -0,0 +1,11 @@
+package kr.co.vcnc.swan.study.hibernate.practice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HibernatePracticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<HibernatePracticeApplication>(*args)
+}
---
+++ src/test/kotlin/kr/co/vcnc/swan/study/hibernate/practice/HibernatePracticeApplicationTests.kt
@@ -0,0 +1,16 @@
+package kr.co.vcnc.swan.study.hibernate.practice
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class HibernatePracticeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
